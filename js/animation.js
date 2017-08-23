eb2080ec8066752b6820c8a3c8a7958c65268e6e
MKhayashi <31233573+MKhayashi@users.noreply.github.com>
2017-08-23 17:59:28 +0900

    commit first

---
+++ js/animation.js
@@ -0,0 +1,14 @@
+// 答え合わせを押したときに呼ばれる
+// 左画面が表示されているかのチェック
+// = データ点の変数がある
+var rCanvas = document.getElementById("tutorial2");
+var rContext = rCanvas.getContext('2d');
+
+// 時間経過によるループでプロット
+while(false){
+  // ユーザの挙動
+  // userPlot();
+  // クラスタリング結果の挙動
+  // resultPlot();
+}
+// sleepで速さを制御
