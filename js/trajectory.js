422862067a3a5e1e36a87cc26f567617fcfd4955
MKhayashi <m143320@edu.tut.ac.jp>
2017-08-25 21:25:37 +0900

    play left

--- js/trajectory.js
+++ js/trajectory.js
@@ -1,28 +1,42 @@
 var colorPosDataForTrajectory = [];
 
 function drawTrajectory(iter){
-console.log(colorPosDataForTrajectory[iter].length+"label");
-  for(var i=0; i<colorPosDataForTrajectory[iter].length; i++){
+  var iterIndex = iter;
+  if(colorPosDataForTrajectory.length <= iter){ 
+    iterIndex = colorPosDataForTrajectory.length - 1;
+  }
+
+console.log(colorPosDataForTrajectory[iterIndex].length+"label");
+  for(var i=0; i<colorPosDataForTrajectory[iterIndex].length; i++){
   //alert(colorPosDataForTrajectory[iter][i].x+"xxxxxxx");
  // alert(colorPosDataForTrajectory[iter][i].g+"iterrrr");
-    drawCircle(i, colorPosDataForTrajectory[iter]);
+    drawCircle(i, colorPosDataForTrajectory[iterIndex]);
   }
+  //alert("tettttttttttt");
 }
 
 //call once
 function setTrajectory(){
-//  var colorPosDataTmp = $.extend(true, {}, colorPosData);
-  var colorPosDataTmp = [];
-  for(var i=0; i<initColorPosData.length; i++){
-    var cx = initColorPosData[i].x;
-    var cy = initColorPosData[i].y;
-    var cr = initColorPosData[i].r;
-    var cnormG = initColorPosData[i].normG;
-    var cnormB = initColorPosData[i].normB;
-	var cid = initColorPosData[i].id;
-    colorPosDataTmp.push(new makeColorPosData(cx, cy, cr, cnormG, cnormB, cid));
-  }
+    function copyColorPosData(argColorPosData){
+	  var colorPosDataTmp = [];
+      for(var i=0; i<argColorPosData.length; i++){
+        var cx = argColorPosData[i].x;
+        var cy = argColorPosData[i].y;
+        var cr = argColorPosData[i].r;
+        var cG = argColorPosData[i].g/255.0;// normG
+        var cB = argColorPosData[i].b/255.0;
+	    var cid = argColorPosData[i].id;
+	    console.log(cG + "," + cB + "sssss"); // undifined
 
+        colorPosDataTmp.push(new makeColorPosData(cx, cy, cr, cG, cB, cid));
+      }
+	  return colorPosDataTmp;
+	}
+
+   colorPosDataForTrajectory.push(copyColorPosData(initColorPosData));
+
+   var colorPosDataTmp = copyColorPosData(initColorPosData);
+   
   //var colorPosDataTmp = colorPosData;
   for(var i = 0; i < userOprHistory.length; i++){
     for(var j = 0; j < colorPosDataTmp.length; j++){
@@ -36,7 +50,9 @@ function setTrajectory(){
       }
     }
 	console.log(colorPosDataTmp[0].x+"xxx");
-    colorPosDataForTrajectory.push(colorPosDataTmp);
+
+    colorPosDataForTrajectory.push(copyColorPosData(colorPosDataTmp));
+	
   }
   //alert("leng"+colorPosDataForTrajectory.length);
   //alert(colorPosDataForTrajectory[0].x);
