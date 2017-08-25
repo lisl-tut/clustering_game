342486ea3afeedba2c6e3767e244ed34c75f44d5
rkonabe <rt.konabe@gmail.com>
2017-08-25 15:19:42 +0900

    test

---
+++ js/trajectory.js
@@ -0,0 +1,16 @@
+var colorPosDataForTrajectory = [];
+
+//colorPosData
+//userOprHistory
+
+function drawTrajectory(iter){
+  for(var i=0; i<colorPosDataForTrajectory.length; i++){
+    drawCircle(i, colorPosDataForTrajectory);
+  }
+}
+
+function setTrajectory(){
+  for(var i = 0; i < u; i++){
+
+  }
+}
